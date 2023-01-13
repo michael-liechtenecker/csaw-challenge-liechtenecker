a1bafc154614e0510106ba200aaddc3bf2bc7392
michael-liechtenecker <120963682+michael-liechtenecker@users.noreply.github.com>
2023-01-13 12:47:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var data = 0;
+
+//increment function
+function increment() {
+    if (data == 0){
+        document.getElementById("warning").innerText = "";
+        document.getElementById("decrementButton").disabled = false;
+    }
+    data = data + 1;
+    document.getElementById("counting").innerText = data;
+    
+}
+
+//decrement function
+function decrement() {
+    if (data > 1){
+        data = data - 1;
+        document.getElementById("counting").innerText = data;
+    }else if (data == 1){
+        data = data - 1;
+        document.getElementById("counting").innerText = data;
+        document.getElementById("decrementButton").disabled = true; 
+        document.getElementById("warning").innerText = "Counter cannot be smaller than 0"; 
+    }
+
+}
